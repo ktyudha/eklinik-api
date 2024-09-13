e04a72aebb902e31129306020208f0cae5476cd3
ktyudha <ktyudha@gmail.com>
2024-09-13 14:41:07 +0700

    add crud classifications

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,20 @@ Route::prefix('v1')->group(function () {
 
                 // Route::apiResource('users', UserController::class);
 
-                // Group
+                // Patients
                 Route::get('/patients', [PatientController::class, 'index']);
                 Route::post('/patients', [PatientController::class, 'store']);
                 Route::get('/patients/{id}', [PatientController::class, 'show']);
                 Route::put('/patients/{id}', [PatientController::class, 'update']);
                 Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
 
-                Route::apiResource('classifications', ClassificationController::class);
+                // Classifications
+                Route::get('/classifications', [ClassificationController::class, 'index']);
+                Route::post('/classifications', [ClassificationController::class, 'store']);
+                Route::get('/classifications/{id}', [ClassificationController::class, 'show']);
+                Route::put('/classifications/{id}', [ClassificationController::class, 'update']);
+                Route::delete('/classifications/{id}', [ClassificationController::class, 'destroy']);
+
                 Route::apiResource('medicals', MedicalController::class);
             });
         }
